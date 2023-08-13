70e38972babf2c970110d7ecbab5d72ed097b66c
iVerkhov <vrkhv@iCloud.com>
2023-08-13 13:53:56 +0300

    Initial Commit

---
+++ Multiplication/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Multiplication
+//
+//  Created by Игорь Верхов on 13.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Multiplication/MultiplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationApp.swift
+//  Multiplication
+//
+//  Created by Игорь Верхов on 13.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
